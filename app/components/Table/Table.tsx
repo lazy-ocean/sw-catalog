84feb735f93d1883a3d32efecd5e8b456d58ca34
Vladlena Panchenko <57011546+lazy-ocean@users.noreply.github.com>
2023-11-12 18:03:31 +0100

    fix: move pagination to a separate component

--- app/components/Table/Table.tsx
+++ app/components/Table/Table.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import {
   createColumnHelper,
   flexRender,
@@ -8,44 +8,47 @@ import {
   useReactTable,
 } from "@tanstack/react-table";
 import { Person, TableProps } from "../../interfaces";
+import { Pagination } from "../Pagination/Pagination";
 
 export const Table = ({ people, planets }: TableProps) => {
   const [data, setData] = useState(people);
   const columnHelper = createColumnHelper<Person>();
 
-  const columns = [
-    // Display Column
-    columnHelper.accessor((row) => row.name, {
-      id: "name",
-      cell: (info) => <i>{info.getValue()}</i>,
-      header: () => <span>Name</span>,
-    }),
-    columnHelper.accessor((row) => row.mass, {
-      id: "mass",
-      cell: (info) => <i>{info.getValue()}</i>,
-      header: () => <span>Mass</span>,
-    }),
-    columnHelper.accessor((row) => row.height, {
-      id: "height",
-      cell: (info) => <i>{info.getValue()}</i>,
-      header: () => <span>Height</span>,
-    }),
-    columnHelper.accessor((row) => row.homeworld, {
-      id: "homeworld",
-      cell: (info) => <i>{info.getValue()}</i>,
-      header: () => <span>Homeworld</span>,
-    }),
-    columnHelper.accessor((row) => row.created, {
-      id: "created",
-      cell: (info) => <i>{info.getValue()}</i>,
-      header: () => <span>Created at</span>,
-    }),
-    columnHelper.accessor((row) => row.edited, {
-      id: "edited",
-      cell: (info) => <i>{info.getValue()}</i>,
-      header: () => <span>Edited at</span>,
-    }),
-  ];
+  const columns = useMemo(
+    () => [
+      columnHelper.accessor((row) => row.name, {
+        id: "name",
+        cell: (info) => <i>{info.getValue()}</i>,
+        header: () => <span>Name</span>,
+      }),
+      columnHelper.accessor((row) => row.mass, {
+        id: "mass",
+        cell: (info) => <i>{info.getValue()}</i>,
+        header: () => <span>Mass</span>,
+      }),
+      columnHelper.accessor((row) => row.height, {
+        id: "height",
+        cell: (info) => <i>{info.getValue()}</i>,
+        header: () => <span>Height</span>,
+      }),
+      columnHelper.accessor((row) => row.homeworld, {
+        id: "homeworld",
+        cell: (info) => <i>{info.getValue()}</i>,
+        header: () => <span>Homeworld</span>,
+      }),
+      columnHelper.accessor((row) => row.created, {
+        id: "created",
+        cell: (info) => <i>{info.getValue()}</i>,
+        header: () => <span>Created at</span>,
+      }),
+      columnHelper.accessor((row) => row.edited, {
+        id: "edited",
+        cell: (info) => <i>{info.getValue()}</i>,
+        header: () => <span>Edited at</span>,
+      }),
+    ],
+    []
+  );
 
   const table = useReactTable({
     data,
@@ -85,62 +88,7 @@ export const Table = ({ people, planets }: TableProps) => {
           ))}
         </tbody>
       </table>
-      <div>
-        <button
-          onClick={() => table.setPageIndex(0)}
-          disabled={!table.getCanPreviousPage()}
-        >
-          {"<<"}
-        </button>
-        <button
-          onClick={() => table.previousPage()}
-          disabled={!table.getCanPreviousPage()}
-        >
-          {"<"}
-        </button>
-        <button
-          onClick={() => table.nextPage()}
-          disabled={!table.getCanNextPage()}
-        >
-          {">"}
-        </button>
-        <button
-          onClick={() => table.setPageIndex(table.getPageCount() - 1)}
-          disabled={!table.getCanNextPage()}
-        >
-          {">>"}
-        </button>
-        <span>
-          <div>Page</div>
-          <strong>
-            {table.getState().pagination.pageIndex + 1} of{" "}
-            {table.getPageCount()}
-          </strong>
-        </span>
-        <span>
-          | Go to page:
-          <input
-            type="number"
-            defaultValue={table.getState().pagination.pageIndex + 1}
-            onChange={(e) => {
-              const page = e.target.value ? Number(e.target.value) - 1 : 0;
-              table.setPageIndex(page);
-            }}
-          />
-        </span>
-        <select
-          value={table.getState().pagination.pageSize}
-          onChange={(e) => {
-            table.setPageSize(Number(e.target.value));
-          }}
-        >
-          {[10, 20, 30, 40, 50].map((pageSize) => (
-            <option key={pageSize} value={pageSize}>
-              Show {pageSize}
-            </option>
-          ))}
-        </select>
-      </div>
+      <Pagination table={table} />
     </>
   );
 };
